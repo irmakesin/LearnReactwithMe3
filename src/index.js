16c0647c7a80b1472e5b410a6c4c801efc684094
irmakesin <esinirmak95@gmail.com>
2021-05-11 12:35:32 +0300

    initalizing

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ if (module.hot) {
 }
 
 class App extends React.Component {
+
+  //initalizing
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+  }
+
+  // React says we have to define render!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
