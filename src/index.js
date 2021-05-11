6b7f47063d665444b56ac33f86585b243dc56425
irmakesin <esinirmak95@gmail.com>
2021-05-11 15:00:22 +0300

    condition

--- src/index.js
+++ src/index.js
@@ -26,13 +26,15 @@ class App extends React.Component {
 
   // React says we have to define render!!
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
-    );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
