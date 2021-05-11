51b5674811ab5790c036e773e61153939c8d73ff
irmakesin <esinirmak95@gmail.com>
2021-05-11 14:52:38 +0300

    err

--- src/index.js
+++ src/index.js
@@ -6,25 +6,33 @@ if (module.hot) {
 }
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
 
-    //initalizing the lat null
-    this.state = { lat: null };
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        this.setState({ lat: position.coords.latitude })
+        // we called setstate!!!!
+        this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // React says we have to define render!!
   render() {
-
-    return <div>Latitude: { this.state.lat } </div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
