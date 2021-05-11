d79202d6877bbf591240b78987a14e08f1d9ee91
irmakesin <esinirmak95@gmail.com>
2021-05-11 11:46:34 +0300

    render

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ if (module.hot) {
   module.hot.accept();
 }
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
 
-  return <div>Hi there!</div>;
-};
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
